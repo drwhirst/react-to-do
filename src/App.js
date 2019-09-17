0f8a657757e5dd273fc1abfb748ac883bb97c74d
David Hirst <dhirst@ametroslearning.com>
2019-09-17 15:38:08 -0400

    adds state to todos

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import ToDo from './components/ToDo.js'
 
-function App() {
-  return (
-    <div className="App">
-      <ToDo />
-      <ToDo />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [
+        { description: 'Walk the cat', isCompleted: true },
+        { description: 'Throw the dishes away', isCompleted: false },
+        { description: 'Buy new dishes', isComplete: false }
+      ]
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <ul>
+          { this.state.todos.map( (todo, index) =>
+            <ToDo key={ index } description={ todo.description } isComplete={ todo.isComplete }/>
+          )}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
